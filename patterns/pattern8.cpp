2b6f116cc1a57fedfa9e56751245b3f61e04e21d
23Bhupesh <bhupeshsahu2312@gmail.com>
2023-06-16 00:48:05 +0530

    Updated File

---
+++ patterns/pattern8.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter the value of n : ";
+    cin>>n;
+
+    int i = 1;
+    int value = 1;
+    while(i<=n) {
+        int space = n-i;
+        while(space) {
+            cout<<" ";
+            space = space - 1;
+        }
+        int j = 1;
+        while(j<=i) {
+            cout<<value;
+            value = value + 1;
+            j = j + 1;
+        }
+        cout<<endl;
+        i = i + 1;
+    }
+}

c80c635e48d0722b2e15f948c05ddd737c071ce8
23Bhupesh <bhupeshsahu2312@gmail.com>
2023-06-26 09:08:36 +0530

    Updated File

---
+++ Basics/scoping.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    // int a = 3;
+    // cout<< a <<endl;
+
+    // if(true) {
+    //     int a = 4;
+    //     cout<< a <<endl;
+    //     int b = 6;
+    //     cout<< b <<endl; 
+    // }
+    // int b = 9;
+
+
+    // cout<< b <<endl;
+
+    // int i = 8;
+    // for(int i = 0; i < 8 ; i++) {
+    //     cout<<"HI"<<endl;
+    // }
+
+    if(true) {
+        int a = 21;
+        cout<< a <<endl;
+        if(true) {
+            int a = 31;
+            cout<< a <<endl;
+            if(true) {
+                
+                cout<< a <<endl;
+            }
+        }
+    }
+
+}
